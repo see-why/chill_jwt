1d71c791830046a979812a0d051c1a1636a1c22c
Osita Cyril Iyadi <Iyadicy@gmail.com>
2025-05-15 09:05:15 -0400

    Add validations for User model in specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe ''
+  describe 'validations' do
+    context 'invalid user' do
+      subject(:user) { build(:user, email: "", password: "") }
+
+      it 'is invalid without email' do
+        user.valid?
+        expect(user.errors[:email]).to include("can't be blank")
+      end
+
+      it 'is invalid without password' do
+        user.valid?
+        expect(user.errors[:password]).to include("can't be blank")
+      end
+    end
+
+    context 'valid user' do
+      subject(:user) { build(:user) }
+
+      it 'email is valid' do
+        user.valid?
+        expect(user.errors[:email]).to be_empty
+      end
+
+      it 'password is valid' do
+        user.valid?
+        expect(user.errors[:password]).to be_empty
+      end
+    end
+  end
 end
