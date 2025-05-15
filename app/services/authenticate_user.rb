40e61d1a931bd9bb6396bad28bda3317194af2f3
Osita Cyril Iyadi <Iyadicy@gmail.com>
2025-05-15 06:13:50 -0400

    Refactor AuthenticateUser service to improve parameter handling and enhance user lookup logic

--- app/services/authenticate_user.rb
+++ app/services/authenticate_user.rb
@@ -1,12 +1,13 @@
-class AuthenticateUser < Services::BaseService
-  def initialize(email, password)
+class AuthenticateUser < BaseService
+  def initialize(email:, password:)
     @email = email
     @password = password
   end
 
   def call
-    user = User.find_by_email(@email)
-    return Response.new(nil, :unauthorized) unless user&.authenticate(@password)
+    user = User.find_by(email: @email)
+    return Response.new(nil, :not_found) unless user
+    return Response.new(nil, :unauthorized) unless user.authenticate(@password)
 
     token = TokenManager::JsonWebTokenManager.encode(user_id: user.id)
     Response.new({ token: token }, nil)
