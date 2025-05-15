ac48d496423a537d54fd60f765e4805ec5e6991a
Osita Cyril Iyadi <Iyadicy@gmail.com>
2025-05-15 05:11:30 -0400

    Update user factory to use sequence for email generation

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :user do
-    email { "MyString" }
+    sequence(:email) { |n| "user#{n}@example.com" }
     password_digest { "MyString" }
+    password_confirmation { "MyString" }
   end
 end
