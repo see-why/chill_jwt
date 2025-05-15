980e1b55e5ba44c9e3c97e56042bdd81cd27c650
Osita Cyril Iyadi <Iyadicy@gmail.com>
2025-05-15 04:15:56 -0400

    Add AuthorizeRequest service for handling user authorization

---
+++ app/services/authorize_request.rb
@@ -0,0 +1,24 @@
+class AuthorizeRequest < Services::BaseService
+  def initialize(headers = {})
+    @headers = headers
+    super()
+  end
+
+  def call
+    return Response.new(nil, :unauthorized) unless decoded_auth_token
+
+    user = User.find(id: decoded_auth_token[:user_id])
+    user ? Response.new({ user: user }, nil) : Response.new(nil, :not_found)
+  end
+
+  private
+
+  def decoded_auth_token
+    @decoded_auth_token ||= JsonWebTokenManager.decode(http_auth_token)
+  end
+
+  def http_auth_token
+    scheme, token = @headers.fetch("Authorization", "")&.split
+    token if scheme == "Bearer"
+  end
+end
