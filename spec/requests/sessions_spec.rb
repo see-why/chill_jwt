3a7455fd45690a66c02584cfe3e543b8517969b6
Osita Cyril Iyadi <Iyadicy@gmail.com>
2025-05-15 06:27:31 -0400

    Refactor Sessions API specs to use subject for request calls and improve context organization

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -4,9 +4,19 @@ RSpec.describe 'Sessions API', type: :request do
   let!(:user) { create(:user, email: "sam@gmail.com") }
 
   describe 'post /v1/sessions' do
+    subject do
+      post '/v1/sessions', params: {
+        email: email,
+        password: password
+      }
+    end
+
     context 'with valid credentails' do
+      let(:password) { "password" }
+      let(:email) { "sam@gmail.com" }
+
       it 'returns a JWT token' do
-        post '/v1/sessions', params: { email: "sam@gmail.com", password: "password" }
+        subject
 
         expect(response).to have_http_status(:ok)
         json = JSON.parse(response.body)
@@ -15,17 +25,26 @@ RSpec.describe 'Sessions API', type: :request do
       end
     end
 
-    context 'with invalid credentials' do
+    context 'with invalid email' do
+      let(:password) {  "yahoo" }
+      let(:email) { "jon@gmail.com" }
+
       it 'returns not found for missing user' do
-        post '/v1/sessions', params: { email: "nobody@now.com", password: "yahoo" }
+        subject
 
         expect(response).to have_http_status(:not_found)
         json = JSON.parse(response.body)
+
         expect(json["errors"]).to be_present
       end
+    end
 
-      it 'returns unauthorized for invalid password' do
-        post '/v1/sessions', params: { email: "sam@gmail.com", password: "harbour" }
+    context 'with invalid password' do
+      let(:password) { "harbour" }
+      let(:email) { "sam@gmail.com" }
+
+      it 'returns unauthorized' do
+        subject
 
         expect(response).to have_http_status(:unauthorized)
         json = JSON.parse(response.body)
